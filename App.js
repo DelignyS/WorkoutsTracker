64fae120d8475af425e05e03d8a52ddc576b01e9
Sylvain.D <60609924+JerkStanton@users.noreply.github.com>
2024-01-31 23:00:22 +0100

    basic README

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import exercises from './assets/data/exercises.json';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import exercises from "./assets/data/exercises.json";
 
 export default function App() {
-
   const exercise = exercises[0];
- 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        Muscle: {exercise.muscle} | Equipment: {exercise.equipment}
-      </Text>
+      <View style= {styles.exerciceContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
 
-      
+        <Text style={styles.exerciseSubtitle}>
+          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+        </Text>
+      </View>
 
       <StatusBar style="auto" />
     </View>
@@ -24,15 +23,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'gainsboro',
+   
+    justifyContent: "center",
+    padding: 10,
+  },
+  exerciceContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
   },
   exerciseName: {
     fontSize: 20,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   exerciseSubtitle: {
-    color: 'dimgray',
+    color: "dimgray",
   },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+### First things first, If you want to use it with an android phone.
+-clone it
+
+
+-on the root of this repository, install all the dependencies 
+
+
+
+-Go to the store and catch Expo Go ©
+- Right after you started the server with npm, you'll have a QR, so scan it with your Expo.
+
